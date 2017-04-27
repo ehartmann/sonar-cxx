dc40e4158a797686b597ed7b3a9467caf1b5a3e3
guwirth <guenter.wirth@etas.com>
2017-04-27 18:32:55 +0200

    try with 'Any'

--- integration-tests/features/steps/test_execution_statistics.py
+++ integration-tests/features/steps/test_execution_statistics.py
@@ -334,12 +334,10 @@ def step_impl(context):
 
 
 def _run_command(context, command):
-    print(BRIGHT + "\ncommand : " + command + RESET_ALL) 
     context.log = "_%s_.log" % context.project
 
     sonarhome = os.environ.get("SONARHOME", None)
     if sonarhome:
-        print(BRIGHT + "\nsonarhome : " + sonarhome + RESET_ALL)
         context.serverlog = sonarlog(sonarhome)
         if getattr(context, "serverlogfd", None) is not None:
             context.serverlogfd.close()
@@ -349,8 +347,6 @@ def _run_command(context, command):
         context.serverlogfd = None
 
     projecthome = os.path.join(TESTDATADIR, context.project)
-    print(BRIGHT + "\nprojecthome : " + projecthome + RESET_ALL)
-    
     with open(context.log, "w") as logfile:
         rc = subprocess.call(command,
                              cwd=projecthome,
